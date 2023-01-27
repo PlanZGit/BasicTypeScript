7785a55694d2408c6c2d1d984eeacfa4583845be
Vang Xiong <vangx709@gmail.com>
2023-01-26 16:13:31 -0900

    added the rest of code

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var message = "Welcome Back";
 console.log(message);
@@ -13,8 +28,8 @@ var sentence = "My name is ".concat(name, "\nI am a beginner in Typescript");
 console.log(sentence);
 var n = null;
 var u = undefined;
-var isNew = null;
-var myName = undefined;
+// let isNew: boolean = null;
+// let myName: string = undefined;
 var list1 = [1, 2, 3];
 var list2 = [1, 2, 3];
 var person1 = ["Chris", 22];
@@ -30,6 +45,65 @@ var randomValue = 10;
 randomValue = true;
 randomValue = "Vishwas";
 var myVariable = 10;
-console.log(myVariable.name);
-myVariable();
-myVariable.toUpperCase(); //type assertion
+function hasName(obj) {
+    return !!obj && typeof obj === "object" && "name" in obj;
+}
+if (hasName(myVariable)) {
+    console.log(myVariable.name);
+}
+// console.log(myVariable.name);
+// myVariable();
+// (myVariable as string).toUpperCase(); //type assertion
+var a;
+a = 10;
+a = true;
+var b = 20;
+var mutiType;
+mutiType = 20;
+mutiType = true;
+var anyType;
+anyType = 20;
+anyType = true;
+function add(num1, num2) {
+    if (num2 === void 0) { num2 = 15; }
+    if (num2) {
+        //num2? can be undefine, if not add num1 + num2
+        return num1 + num2;
+    }
+    return num1;
+}
+add(5, 10); //return 15
+add(5); //return 15
+function fullName(person) {
+    console.log("".concat(person.firstName, " ").concat(person.lastName));
+}
+var p = {
+    firstName: "Bruce"
+};
+fullName(p);
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good Morning ".concat(this.employeeName));
+    };
+    return Employee;
+}());
+var emp1 = new Employee("Vishwas");
+console.log(emp1.employeeName);
+emp1.greet();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager delegate tasks");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager("Bruce");
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
--- main.ts
+++ main.ts
@@ -21,8 +21,8 @@ console.log(sentence);
 let n: null = null;
 let u: undefined = undefined;
 
-let isNew: boolean = null;
-let myName: string = undefined;
+// let isNew: boolean = null;
+// let myName: string = undefined;
 
 let list1: number[] = [1, 2, 3];
 let list2: Array<number> = [1, 2, 3];
@@ -43,6 +43,86 @@ randomValue = "Vishwas";
 
 let myVariable: unknown = 10;
 
-console.log(myVariable.name);
-myVariable();
-(myVariable as string).toUpperCase(); //type assertion
+function hasName(obj: any): obj is { name: string } {
+  return !!obj && typeof obj === "object" && "name" in obj;
+}
+
+if (hasName(myVariable)) {
+  console.log(myVariable.name);
+}
+
+// console.log(myVariable.name);
+// myVariable();
+// (myVariable as string).toUpperCase(); //type assertion
+
+let a;
+a = 10;
+a = true;
+
+let b = 20;
+
+let mutiType: number | boolean;
+mutiType = 20;
+mutiType = true;
+
+let anyType: any;
+anyType = 20;
+anyType = true;
+
+function add(num1: number, num2: number = 15): number {
+  if (num2) {
+    //num2? can be undefine, if not add num1 + num2
+    return num1 + num2;
+  }
+  return num1;
+}
+
+add(5, 10); //return 15
+add(5); //return 15
+// add(5, "10");
+
+interface Person {
+  firstName: string;
+  lastName?: string; //optional
+}
+
+function fullName(person: Person) {
+  console.log(`${person.firstName} ${person.lastName}`);
+}
+
+let p = {
+  firstName: "Bruce",
+  // lastName: "Wayne",
+};
+
+fullName(p);
+
+class Employee {
+  public employeeName: string;
+
+  constructor(name: string) {
+    this.employeeName = name;
+  }
+
+  greet() {
+    console.log(`Good Morning ${this.employeeName}`);
+  }
+}
+
+let emp1 = new Employee("Vishwas");
+console.log(emp1.employeeName);
+emp1.greet();
+
+class Manager extends Employee {
+  constructor(managerName: string) {
+    super(managerName);
+  }
+  delegateWork() {
+    console.log(`Manager delegate tasks`);
+  }
+}
+
+let m1 = new Manager("Bruce");
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
