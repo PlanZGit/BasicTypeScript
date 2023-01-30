14778a10dd58bf6ca142d0c71981ed187bcda0cd
Vang Xiong <vangx709@gmail.com>
2023-01-30 09:18:57 -0900

    finishing basic

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
+var __extends =
+  (this && this.__extends) ||
+  (function () {
     var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
+      extendStatics =
+        Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array &&
+          function (d, b) {
+            d.__proto__ = b;
+          }) ||
+        function (d, b) {
+          for (var p in b)
+            if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p];
+        };
+      return extendStatics(d, b);
     };
     return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+      if (typeof b !== "function" && b !== null)
+        throw new TypeError(
+          "Class extends value " + String(b) + " is not a constructor or null"
+        );
+      extendStatics(d, b);
+      function __() {
+        this.constructor = d;
+      }
+      d.prototype =
+        b === null
+          ? Object.create(b)
+          : ((__.prototype = b.prototype), new __());
     };
-})();
+  })();
 exports.__esModule = true;
 var message = "Welcome Back";
 console.log(message);
@@ -35,9 +51,9 @@ var list2 = [1, 2, 3];
 var person1 = ["Chris", 22];
 var Color;
 (function (Color) {
-    Color[Color["Red"] = 5] = "Red";
-    Color[Color["Green"] = 6] = "Green";
-    Color[Color["Blue"] = 7] = "Blue";
+  Color[(Color["Red"] = 5)] = "Red";
+  Color[(Color["Green"] = 6)] = "Green";
+  Color[(Color["Blue"] = 7)] = "Blue";
 })(Color || (Color = {}));
 var c = Color.Green;
 console.log(c);
@@ -46,10 +62,10 @@ randomValue = true;
 randomValue = "Vishwas";
 var myVariable = 10;
 function hasName(obj) {
-    return !!obj && typeof obj === "object" && "name" in obj;
+  return !!obj && typeof obj === "object" && "name" in obj;
 }
 if (hasName(myVariable)) {
-    console.log(myVariable.name);
+  console.log(myVariable.name);
 }
 // console.log(myVariable.name);
 // myVariable();
@@ -65,44 +81,46 @@ var anyType;
 anyType = 20;
 anyType = true;
 function add(num1, num2) {
-    if (num2 === void 0) { num2 = 15; }
-    if (num2) {
-        //num2? can be undefine, if not add num1 + num2
-        return num1 + num2;
-    }
-    return num1;
+  if (num2 === void 0) {
+    num2 = 15;
+  }
+  if (num2) {
+    //num2? can be undefine, if not add num1 + num2
+    return num1 + num2;
+  }
+  return num1;
 }
 add(5, 10); //return 15
 add(5); //return 15
 function fullName(person) {
-    console.log("".concat(person.firstName, " ").concat(person.lastName));
+  console.log("".concat(person.firstName, " ").concat(person.lastName));
 }
 var p = {
-    firstName: "Bruce"
+  firstName: "Bruce",
 };
 fullName(p);
 var Employee = /** @class */ (function () {
-    function Employee(name) {
-        this.employeeName = name;
-    }
-    Employee.prototype.greet = function () {
-        console.log("Good Morning ".concat(this.employeeName));
-    };
-    return Employee;
-}());
+  function Employee(name) {
+    this.employeeName = name;
+  }
+  Employee.prototype.greet = function () {
+    console.log("Good Morning ".concat(this.employeeName));
+  };
+  return Employee;
+})();
 var emp1 = new Employee("Vishwas");
 console.log(emp1.employeeName);
 emp1.greet();
 var Manager = /** @class */ (function (_super) {
-    __extends(Manager, _super);
-    function Manager(managerName) {
-        return _super.call(this, managerName) || this;
-    }
-    Manager.prototype.delegateWork = function () {
-        console.log("Manager delegate tasks");
-    };
-    return Manager;
-}(Employee));
+  __extends(Manager, _super);
+  function Manager(managerName) {
+    return _super.call(this, managerName) || this;
+  }
+  Manager.prototype.delegateWork = function () {
+    console.log("Manager delegate tasks");
+  };
+  return Manager;
+})(Employee);
 var m1 = new Manager("Bruce");
 m1.delegateWork();
 m1.greet();
